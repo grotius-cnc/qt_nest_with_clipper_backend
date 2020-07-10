50c83ba218fce59ecf62e33464eeaa2fe7b72152
grotius-cnc <44880102+grotius-cnc@users.noreply.github.com>
2020-07-10 12:23:41 -0400

    Add files via upload

---
+++ nlopt/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/QT_Nest/nlopt
+# Build directory: /home/user/QT_Nest/nlopt
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src/api")
+subdirs("test")
